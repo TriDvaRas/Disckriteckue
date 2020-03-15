47ecdb6d9ff10bc5b9f24bb5aec27787f9c22220
TriDvaRas <02dimon02@gmail.com>
2020-03-15 18:49:37 +0200

    Changed index
    Now reads shape data from file

--- index.js
+++ index.js
@@ -1,51 +1,117 @@
+//globals
+CrcNum = 2;
+
+//preload figures data into globals
+function LoadFigures() {
+    GetShapes(2).then((data) => {
+        ShapeList2 = data;
+        DrawCanvas();
+    });
+    GetShapes(3).then((data) => {
+        ShapeList3 = data;
+    });
+}
+
+//
 function okPressed() {
     text = document.getElementById("Form").value;
     console.log(text);
 }
-function DrawCircles(num) {
+
+//radio button change
+function ChangeCrcNum(num) {
+    if (CrcNum != num) {
+        CrcNum = num;
+        DrawCanvas();
+    }
+}
+
+//redraw canvas
+function DrawCanvas() {
     var drawingCanvas = document.getElementById('mainCanvas');
     var context = drawingCanvas.getContext('2d');
     if (drawingCanvas && drawingCanvas.getContext) {
         context.clear();
-        if (num == 2) {
-            context.strokeStyle = "#000";
-            context.lineWidth = 2;
-            // 1 
-            context.beginPath();
-            context.arc(225, 300, 200, 0, Math.PI * 2, true);
-            context.closePath();
-            context.stroke();
-            //2
-            context.beginPath();
-            context.arc(375, 300, 200, 0, Math.PI * 2, true);
-            context.closePath();
-            context.stroke();
-        } else if (num == 3) {
-            context.strokeStyle = "#000";
-            context.lineWidth = 2;
-            // 1 
-            context.beginPath();
-            context.arc(225, 225, 200, 0, Math.PI * 2, true);
-            context.closePath();
-            context.stroke();
-            //2
-            context.beginPath();
-            context.arc(375, 225, 200, 0, Math.PI * 2, true);
-            context.closePath();
-            context.stroke();
-            //3
-            context.beginPath();
-            context.arc(300, 375, 200, 0, Math.PI * 2, true);
-            context.closePath();
-            context.stroke();
-
-        } else if (num == 4) {
+        if (window.CrcNum == 2) {
+            DrawShapeList(context, window.ShapeList2);
+        } else if (window.CrcNum == 3) {
+            DrawShapeList(context, window.ShapeList3);
+        } else if (window.CrcNum == 4) {
 
         }
 
 
     }
 }
+//DrawShapes
+function DrawShapeList(canvas, ShapeList) {
+    canvas.strokeStyle = "#000";
+    for (let i = 0; i < ShapeList.Objects.length; i++) {
+        const A = ShapeList.Objects[i];
+        canvas.lineWidth = A.strokeWidth;
+        canvas.beginPath();
+        canvas.lineCap = "round";
+        console.log(A);
+        //if (A.name != "B") continue;
+
+
+        for (let index = 0; index < A.path.length; index++) {
+            const P = A.path[index];
+            console.log(P.params);
+
+            if (P.type == "arc") {
+                canvas.arc(P.params[0], P.params[1], P.params[2], P.params[3], P.params[4], P.params[5]);
+            }
+            else if (P.type == "line") {
+                canvas.moveTo(P.params[0], P.params[1]);
+                canvas.lineTo(P.params[2], P.params[3]);
+            }
+            else if (P.type == "rect") {
+                if (A.fill) {
+                    canvas.fillStyle = "rgb(200,200,200)";
+                    canvas.fillRect(P.params[0], P.params[1], P.params[2], P.params[3]);
+                }
+                canvas.strokeRect(P.params[0], P.params[1], P.params[2], P.params[3]);
+
+            }
+        }
+
+        canvas.closePath();
+        if (A.fill) {
+            canvas.fillStyle = "rgb(220,220,220)";
+        } else {
+            canvas.fillStyle = "rgb(255,255,255)";
+        }
+
+        canvas.fill();
+        canvas.stroke();
+        if (A.labelPos) {
+            canvas.font = "42px serif";
+            canvas.fillStyle = "rgb(0,0,0)";
+            canvas.fillText(A.name, A.labelPos[0], A.labelPos[1])
+        }
+    }
+}
+
+//get shapelist data
+function GetShapes(num) {
+    return new Promise(function (resolve, reject) {
+        var request = new XMLHttpRequest();
+        request.open('GET', `https://tridvaras.github.io/Disckriteckue/Shapes/${num}.json`);
+        request.responseType = 'json';
+        request.send();
+
+        request.onload = function () {
+            data = request.response;
+            console.log(data);
+
+            resolve(data);
+        }
+    });
+
+}
+
+//canvas clearing method
 CanvasRenderingContext2D.prototype.clear =
     CanvasRenderingContext2D.prototype.clear || function (preserveTransform) {
         if (preserveTransform) {
